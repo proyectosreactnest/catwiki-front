27b6929595575a25f956e66a19fa96ac55d39e29
Onnichan <dyangeltk17@gmail.com>
2023-05-04 22:51:02 -0500

    chore: add msw for mocking http requests

---
+++ mocks/server/handlers.ts
@@ -0,0 +1,13 @@
+import {rest} from "msw"
+
+export const handlers = [
+  rest.get("/task/status", async (_req,res, ctx) => {
+    return await res(
+      ctx.json([
+        {
+          statusCode: 200,
+        }
+      ])
+    )
+  })
+]
